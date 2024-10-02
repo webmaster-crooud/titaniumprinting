ff86c588103cb2b637db35bfbc06627ac440b64f
Mikael Aditya N <mikaeladityan.99@gmail.com>
2024-10-02 10:57:05 +0700

    Feature: Categories [DONE]

--- src/validations/category.valdation.js
+++ src/validations/category.valdation.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 export const categoryValidation = Joi.object({
 	name: Joi.string().min(5).max(100).required(),
 	slug: Joi.string().min(5).max(100).optional(),
-	description: Joi.string().max(1000).optional(),
+	description: Joi.string().max(1000).optional().allow(""),
 });
 
 export const getCategoryValidation = Joi.number().required();
